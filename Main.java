7466596ec98981e43fde1ba08667e5a3204a5441
IvanSergeev-me <57621521+IvanSergeev-me@users.noreply.github.com>
2019-12-16 00:04:23 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,28 +1,37 @@
-package pack;
+  
 import java.util.Scanner;
 
-public class Main {
-	public static void main(String[] args) {
+public class Recursion {
+	static long exponentN(long a,long n) {
+		if (n>0) a = a*(exponentN(a,n-1));
+		else if(n==0) {
+			
+			return 1;
+		}
+		return a;
+	}
+	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int k= sc.nextInt();
-		System.out.println(slo(n,k,0));
-	
+		System.out.println("Введите a: ");
+		long a = sc.nextLong();
+		System.out.println("Введите n: ");
+		long n = sc.nextLong();
+		System.out.println(exponentN(a,n));
+		sc.close();
 	}
-		public static int fact(int n) {
-			if(n==1) return 1;
-			else {
-				return n*fact(n-1);
+
+
+	public static long gcd(long x,long y) {
+		if (y>x) {
+			long p;
+			p=x;
+			x=y;
+			y=p;
+			
 		}
+		if (x==1|y==1) return 1;
+		else if  (y==0) return x;
+		else{ return gcd(y,x%y);
 	}
-		public static int fib(int n) {
-			if(n==0) return 0;
-			else if(n==1) return 1;
-			else return fib(n-1)+fib(n-2);
-		}
-		public static int slo(int n, int k,int s) {
-			if (n==0 || k==0) return 0;
-			if(k==1) return 1;
-			else return s+slo(n,k-1,s);
-		}
+}
 }
